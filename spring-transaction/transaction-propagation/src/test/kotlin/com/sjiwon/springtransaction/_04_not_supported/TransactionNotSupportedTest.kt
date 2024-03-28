1aca03be62a4bc96b3afd3da5c2d5541328578d7
sjiwon <sjiwon4491@gmail.com>
2024-03-28 15:00:46 +0900

    Propagation NOT_SUPPORTED 트랜잭션 테스트

---
+++ spring-transaction/transaction-propagation/src/test/kotlin/com/sjiwon/springtransaction/_04_not_supported/TransactionNotSupportedTest.kt
@@ -0,0 +1,22 @@
+package com.sjiwon.springtransaction._04_not_supported
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestConstructor
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class TransactionNotSupportedTest(
+    private val main: NotSupportedMainComponent,
+) {
+    @Test
+    fun `기존 트랜잭션 X`() {
+        shouldNotThrowAny { main.case1() }
+    }
+
+    @Test
+    fun `기존 트랜잭션 O`() {
+        shouldNotThrowAny { main.case2() }
+    }
+}
