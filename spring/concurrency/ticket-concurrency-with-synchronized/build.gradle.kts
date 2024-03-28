dba4c733fda503e8aadd67d731b910b65911e8da
sjiwon <sjiwon4491@gmail.com>
2024-03-28 21:55:33 +0900

    init

---
+++ spring/concurrency/ticket-concurrency-with-synchronized/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation(project(":common-resources"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
