70a686f947c27a3ed66db002725f49b44b47c42e
sjiwon <sjiwon4491@gmail.com>
2024-03-29 15:51:57 +0900

    `@Transactional` 티켓 정합성 보장 X

---
+++ spring/concurrency/ticket-concurrency-with-synchronized/src/test/kotlin/com/sjiwon/concurrency/common/DatabaseCleaner.kt
@@ -0,0 +1,33 @@
+package com.sjiwon.concurrency.common
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import jakarta.persistence.Table
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class DatabaseCleaner(
+    @PersistenceContext
+    private val entityManager: EntityManager,
+) {
+    private val tableNames: List<String> =
+        entityManager.metamodel
+            .entities
+            .map { it.javaType }
+            .map { it.getAnnotation(Table::class.java) }
+            .map { it.name }
+
+    @Transactional
+    fun cleanUpDatabase() {
+        entityManager.clear()
+        entityManager.flush()
+        entityManager.createNativeQuery("SET foreign_key_checks = 0").executeUpdate()
+
+        for (tableName in tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE $tableName").executeUpdate()
+        }
+
+        entityManager.createNativeQuery("SET foreign_key_checks = 1").executeUpdate()
+    }
+}
