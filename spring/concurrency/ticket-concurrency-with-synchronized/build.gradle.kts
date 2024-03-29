7d15c350af165e62d6c15f1e4299e5a48786cdb6
sjiwon <sjiwon4491@gmail.com>
2024-03-29 17:37:13 +0900

    Nginx + WAS + RDB(MySQL) docker-compose 기반 로드밸런싱 환경

--- spring/concurrency/ticket-concurrency-with-synchronized/build.gradle.kts
+++ spring/concurrency/ticket-concurrency-with-synchronized/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(project(":common-resources"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j:${property("mysqlVersion")}")
     runtimeOnly("com.h2database:h2")
 }
 
