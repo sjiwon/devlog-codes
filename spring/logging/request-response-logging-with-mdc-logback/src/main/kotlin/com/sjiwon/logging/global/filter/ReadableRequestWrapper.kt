8c85f37adbf91c8445cce714e33fb9948c464b53
sjiwon <sjiwon4491@gmail.com>
2024-03-30 16:15:58 +0900

    Request 데이터 캐싱 Wrapper 커스터마이징

---
+++ spring/logging/request-response-logging-with-mdc-logback/src/main/kotlin/com/sjiwon/logging/global/filter/ReadableRequestWrapper.kt
@@ -0,0 +1,65 @@
+package com.sjiwon.logging.global.filter
+
+import jakarta.servlet.ReadListener
+import jakarta.servlet.ServletInputStream
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletRequestWrapper
+import jakarta.servlet.http.Part
+import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
+import java.io.ByteArrayInputStream
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+
+class ReadableRequestWrapper(
+    request: HttpServletRequest,
+) : HttpServletRequestWrapper(request) {
+    private val params: MutableMap<String, Array<String>> = request.parameterMap
+    private val encoding: Charset
+    private val parts: Collection<Part>?
+    val contentAsByteArray: ByteArray
+
+    init {
+        this.encoding = getEncoding(request.characterEncoding)
+        this.parts = getMultipartParts(request)
+
+        try {
+            this.contentAsByteArray = request.inputStream.readAllBytes()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+    private fun getEncoding(charEncoding: String): Charset {
+        if (charEncoding.isBlank()) {
+            return StandardCharsets.UTF_8
+        }
+        return Charset.forName(charEncoding)
+    }
+
+    private fun getMultipartParts(request: HttpServletRequest): Collection<Part>? {
+        if (isMultipartRequest(request)) {
+            return request.parts
+        }
+        return null
+    }
+
+    private fun isMultipartRequest(request: HttpServletRequest): Boolean {
+        return !request.contentType.isNullOrBlank() && request.contentType.startsWith(MULTIPART_FORM_DATA_VALUE)
+    }
+
+    override fun getInputStream(): ServletInputStream {
+        val byteArrayInputStream = ByteArrayInputStream(this.contentAsByteArray)
+
+        return object : ServletInputStream() {
+            override fun isFinished(): Boolean =
+                throw UnsupportedOperationException("[ReadableRequestWrapper] isFinished() not supported")
+
+            override fun isReady(): Boolean =
+                throw UnsupportedOperationException("[ReadableRequestWrapper] isReady() not supported")
+
+            override fun setReadListener(listener: ReadListener) = Unit
+
+            override fun read(): Int = byteArrayInputStream.read()
+        }
+    }
+}
