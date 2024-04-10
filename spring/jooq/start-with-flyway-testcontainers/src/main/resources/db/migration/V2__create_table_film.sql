328809ddd15e24c6c4fe36d952e3c585771532fb
sjiwon <sjiwon4491@gmail.com>
2024-04-10 21:38:59 +0900

    release_Date -> release_date

--- spring/jooq/start-with-flyway-testcontainers/src/main/resources/db/migration/V2__create_table_film.sql
+++ spring/jooq/start-with-flyway-testcontainers/src/main/resources/db/migration/V2__create_table_film.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS film
     `id`           BIGINT AUTO_INCREMENT PRIMARY KEY,
     `title`        VARCHAR(100) NOT NULL,
     `description`  VARCHAR(100) NOT NULL,
-    `release_Date` DATE         NOT NULL,
+    `release_date` DATE         NOT NULL,
     `length`       INT          NOT NULL
 ) ENGINE = InnoDB
   CHARSET = UTF8MB4;
